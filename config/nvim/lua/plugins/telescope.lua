6cc84aa9f622284fb314426b388327456ce1a4f2
Erik Olson <erikhakonolson@gmail.com>
2025-09-20 09:43:36 -0700

    update telescope to lazy load

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -3,17 +3,18 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.4",
   dependencies = { "nvim-lua/plenary.nvim" },
+
+  -- Lazy triggers
+  cmd = "Telescope",  -- :Telescope command loads it
+  keys = {
+    { "<leader>ff", function() require("telescope.builtin").find_files({ hidden = true }) end, desc = "[F]ind [F]iles" },
+    { "<leader>fg", function() require("telescope.builtin").live_grep() end,               desc = "[F]ind with [G]rep" },
+    { "<leader>fb", function() require("telescope.builtin").buffers() end,                 desc = "[F]ind [B]uffers" },
+    { "<leader>fh", function() require("telescope.builtin").help_tags() end,               desc = "[F]ind [H]elp"    },
+  },
+
   config = function()
-    local telescope = require("telescope")
-    local builtin = require("telescope.builtin")
-
-    telescope.setup()
-
-    -- Telescope keybindings
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind with [G]rep" })
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffers" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+    require("telescope").setup({})
   end,
 }
 
